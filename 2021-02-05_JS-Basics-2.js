2fb9ca885924155f7aec2e66e238d2c7230d2635
Christy Cooke <cleecooke@gmail.com>
2021-03-03 19:29:59 -0700

    JS Homework number 2

---
+++ 2021-02-05_JS-Basics-2.js
@@ -0,0 +1,19 @@
+// grades
+
+var jennysGrade  = 54;
+
+if (jennysGrade >= 90) {
+    console.log("That's an A Jenny!");
+} else if (jennysGrade >= 80 && jennysGrade < 90) {
+    console.log("Jenny got a B!");
+} else if (jennysGrade >= 70 && jennysGrade < 80) {
+    console.log("That's a passing C!");
+} else if (jennysGrade >= 60 && jennysGrade < 70) {
+    console.log("Only a D, you can do better!");
+} else if (jennysGrade < 60) {
+    console.log("An F! You failed!");
+}
+
+function getGrade (grade) {
+    
+}
